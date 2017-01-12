78ec6cbefd2f61e4141c4d24a8b9be595fdb9e8c
vsmysle <kyrylo.voronchenko@gmail.com>
2017-01-12 18:18:35 +0000

    add js from departments visualizing

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, jsonify
+import json
 @app.route('/')
 @app.route('/index')
 def index():
+    get_data
     return render_template('index.html')
+
+@app.route('/get_data')
+def get_data():
+    with open("app/data/factory_layout.json") as json_data:
+        return jsonify(json.load(json_data))
