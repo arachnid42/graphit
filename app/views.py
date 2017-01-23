7064709671496a9760def367a645a5a062d1bab2
ddnomad <ddnomad@protonmail.com>
2017-01-23 22:51:46 +0200

    getting ready to merge

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.core.json_assembler import *
 
 
@@ -13,4 +13,12 @@ def index():
 @app.route('/get_data')
 def get_data():
     ja = JSONAssembler(app.root_path+'/core/config.json', force_rebuild=True)
+    return ja.get_viz_json()
+
+
+@app.route('/get_data_filtered')
+def get_data_filtered():
+    date_from = request.args.get('start', None, type=str).split('.').replace('T', ' ', 1)
+    date_to = request.args.get('end', None, type=str).split('.').replace('T', ' ', 1)
+    ja = JSONAssembler(app.root_path+'/core/config.json', force_rebuild=True, date_boundaries=[date_from, date_to])
     return ja.get_viz_json()
