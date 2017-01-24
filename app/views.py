7d4d7f846f36d601f64c101e44a7a25e80c71532
ddnomad <ddnomad@protonmail.com>
2017-01-24 03:34:26 +0200

    beta 0.12 for indoor use only (fixed minor bug)

--- app/views.py
+++ app/views.py
@@ -17,8 +17,9 @@ def get_data():
 
 @app.route('/get_data_filtered')
 def get_data_filtered():
-    dummy_time = " 00:00:00"
-    date_from = request.args.get('start', None, type=str) + dummy_time
-    date_to = request.args.get('end', None, type=str) + dummy_time
+    dummy_time_1 = " 00:00:00"
+    dummy_time_2 = " 23:59:59"
+    date_from = request.args.get('start', None, type=str) + dummy_time_1
+    date_to = request.args.get('end', None, type=str) + dummy_time_2
     ja = JSONAssembler(app.root_path+'/core/config.json', force_rebuild=True, date_boundaries=[date_from, date_to])
     return ja.get_viz_json()
